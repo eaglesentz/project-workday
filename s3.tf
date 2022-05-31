dbdeb7796dd019b42b3120a94badfd6ed1f7f389
jibolaolu <seun_2006@yahoo.com>
2022-05-31 02:17:35 +0100

    First Workday push

---
+++ s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "proj_s3_buckets" {
+  count         = length(var.domain_name)
+  bucket        = var.domain_name[count.index]
+  force_destroy = true
+  tags = merge(var.common_tags, {
+    Name = "${var.service_name}-s3-bucket"
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "region" {
+  type        = string
+  description = "AWS region name"
+}
+variable "vpc_cidr" {
+  type = string
+}
+variable "service_name" {
+  description = "The name of the network"
+  default     = "prj-dev"
+}
+variable "public_subnet_cidr" {
+  type = list(string)
+}
+variable "public_subnet_names" {
+  type = list(string)
+}
+variable "private_subnet_cidr" {
+  type = list(string)
+}
+variable "private_subnet_names" {
+  type = list(string)
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami" {
+  default = "ami-00f6a0c18edb19300"
+}
+variable "availability_zone" {
+  type = list(string)
+}
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+variable "domain_name" {
+  type = list(string)
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    project     = "Workday"
+    environment = "Dev"
+    team        = "Platform Engineering"
+  }
+}
