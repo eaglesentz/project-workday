ac474e22695a45e4b1d76bc7b3aec093196519ce
cenots02 <cenots02@UNKNOWN.Home>
2022-06-10 05:26:38 +0100

    Adding buket region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "workdayproj-tfstate"
     key            = "workdayproj.tfstate"
-    region         = "eu-west-2"
+    region         = "eu-west-1"
     dynamodb_table = "workday-terraform-lock-state"
     encrypt        = true
   }
