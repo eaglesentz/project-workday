30defdf9a641f544ecad27d83486cd6e78344719
cenots02 <cenots02@UNKNOWN.Home>
2022-06-15 02:10:56 +0100

    Adding alb arn

--- asg.tf
+++ asg.tf
@@ -7,7 +7,7 @@ resource "aws_autoscaling_group" "asg" {
   health_check_grace_period = 300
   health_check_type         = "ELB"
   vpc_zone_identifier       = aws_subnet.private_subnet.*.id
-  target_group_arns         = [aws_alb_target_group.albtg.arn]
+  target_group_arns         = [aws_alb_target_group.workday-albtg.arn]
 
   tag {
     key                 = "Name"
